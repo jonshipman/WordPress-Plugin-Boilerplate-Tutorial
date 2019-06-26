dbc6661d4b5e45a97f6634806e35ee23ac48aa7c
jonshipman <jon@jonshipman.com>
2019-06-26 14:32:16 -0500

    Update custom_post_types.php
    Instead of requiring the post_types class twice, how about using a boolean filter that's default false, but is set to true on activation? Change should also be made to .\plugin-name\includes\class-plugin-name-post_types.php

--- plugin-name/tutorials/custom_post_types.php
+++ plugin-name/tutorials/custom_post_types.php
@@ -30,13 +30,6 @@ private function define_admin_hooks() {
     $plugin_post_types = new Plugin_Name_Post_Types();
 
     /**
-     * The problem with the initial activation code is that when the activation hook runs, it's after the init hook has run,
-     * so hooking into init from the activation hook won't do anything.
-     * You don't need to register the CPT within the activation function unless you need rewrite rules to be added
-     * via flush_rewrite_rules() on activation. In that case, you'll want to register the CPT normally, via the
-     * loader on the init hook, and also re-register it within the activation function and
-     * call flush_rewrite_rules() to add the CPT rewrite rules.
-     *
      * @link https://github.com/DevinVinson/WordPress-Plugin-Boilerplate/issues/261
      */
     $this->loader->add_action( 'init', $plugin_post_types, 'create_custom_post_type', 999 );
@@ -48,43 +41,10 @@ private function define_admin_hooks() {
 
 public static function activate() {
 
-    // ...
-
     /**
-     * Custom Post Types
-     */
-    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-plugin-name-post_types.php';
-    $plugin_post_types = new Plugin_Name_Post_Types();
-
-    /**
-     * The problem with the initial activation code is that when the activation hook runs, it's after the init hook has run,
-     * so hooking into init from the activation hook won't do anything.
-     * You don't need to register the CPT within the activation function unless you need rewrite rules to be added
-     * via flush_rewrite_rules() on activation. In that case, you'll want to register the CPT normally, via the
-     * loader on the init hook, and also re-register it within the activation function and
-     * call flush_rewrite_rules() to add the CPT rewrite rules.
-     *
-     * @link https://github.com/DevinVinson/WordPress-Plugin-Boilerplate/issues/261
-     */
-    $plugin_post_types->create_custom_post_type();
-
-    /**
-     * This only required if custom post type has rewrite!
-     *
-     * Remove rewrite rules and then recreate rewrite rules.
-     *
-     * This function is useful when used with custom post types as it allows for automatic flushing of the WordPress
-     * rewrite rules (usually needs to be done manually for new custom post types).
-     * However, this is an expensive operation so it should only be used when absolutely necessary.
-     * See Usage section for more details.
-     *
-     * Flushing the rewrite rules is an expensive operation, there are tutorials and examples that suggest
-     * executing it on the 'init' hook. This is bad practice. It should be executed either
-     * on the 'shutdown' hook, or on plugin/theme (de)activation.
-     *
-     * @link https://codex.wordpress.org/Function_Reference/flush_rewrite_rules
-     */
-    flush_rewrite_rules();
+     * Add a filter to pass along to the post_type class
+     **/
+    add_filter( 'plugin-name-post_types-rewrite-flush', '__return_true' );
 
 }
 
@@ -260,6 +220,10 @@ class Plugin_Name_Post_Types {
             }
 
         }
+	    
+	if ( apply_filters( 'plugin-name-post_types-rewrite-flush', false ) ) {
+		flush_rewrite_rules();
+	}
 
     }
 
